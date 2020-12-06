fe8514ef3f643ddef78ed08c46df9e6f9e6bfde1
n34217916 <deryakarakaya@std.iyte.edu.tr>
2020-12-06 17:57:53 +0000

    1

---
+++ lab7/example5.py
@@ -0,0 +1,28 @@
+#import string
+#lc = string.ascii_lowercase
+#lc_list = list(lc)
+#uc = string.ascii_uppercase
+#uc_list = list(uc)
+#numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#password = input("Enter the password: ")
+#password = list(password)
+
+#for i in password:
+ # if i in lc_list:
+  #  for s in password:
+   #   if s in uc_list:
+    #    for k in password:
+     #     if k in numbers:
+      #      a = True
+      #      break
+       #   else:
+        #    continue
+     # else:
+      #  continue
+  #else:
+   # continue
+
+#if a is True:
+ # print("Your password is valid.")
+#else:
+ # print("Your password is invalid. Make sure it has at least one uppercase letter, one lowecase letter and one number.")
--- lab7/example6.py
+++ lab7/example6.py
@@ -3,10 +3,18 @@ numbers2 = [10,20,20,15,30,40]
 s1 = set(numbers1)
 s2 = set(numbers2)
 intersection = []
-if s in s1 and s in s2:
-  intersection.append(s)
-  s1.remove(s)
-  s2.remove(s)
-else:
-  pass
-print(intersection)
+for s in s1:
+  if s in s1 and s in s2:
+    intersection.append(s)
+  else:
+    pass
+print("Intersection of the two sets is: ", intersection)
+union = []
+for i in s1:
+  union.append(i)
+for s in s2:
+  if s not in union:
+    union.append(s)
+  else:
+    pass
+print("Union of the two sets is: ", union)
